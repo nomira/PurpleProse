dcbad8b4a0132202ed9bb84d8a3a46acadd1c252
CastonYoung <caston.young@oit.edu>
2016-11-08 19:13:42 -0800

    Add files via upload

--- Object.cs
+++ Object.cs
@@ -7,96 +7,93 @@ using System.IO;
 
 namespace StoryPlanner
 {
-    public abstract class Object  //This will be a parent class to all objects
-    {
-        public enum relationshipTypes {Null, Father, Mother, Sibling, Friend, Enemy}; //Types of relationships... let me know if we need more
-        protected string name, description, history, imageFile;
-        protected List<relationship> myRelationships; //List of this objects relationships
-        public Object(string name, string desc, string hist, string imageFile) { //Constructor to assign values to name, desc, and hist
-            this.name = name;
-            this.description = desc; //desc, hist, and imageFile hold filenames with extension
-            this.history = hist;
-            this.imageFile = imageFile;
+	public abstract class Object  //This will be a parent class to all objects
+	{
+		public enum relationshipTypes {Null, Father, Mother, Sibling, Friend, Enemy}; //Types of relationships... let me know if we need more
+		protected string name, description, history, imageFile;
+		protected List<relationship> myRelationships; //List of this objects relationships
+		
+		public Object(string name, string desc, string hist, string image_file)
+			{ InitializerA(	 name,		  desc,		   hist,		image_file); }
+
+		private InitializerA(string name, string desc, string hist, string image_file)
+		{	attribute.name = name;
+            attribute.description = desc; //Desc, hist, and imageFile hold filenames with the extension
+            attribute.history = hist;
+            this.imageFile = image_file;
             myRelationships = new List<relationship>();
         }
-        protected struct relationship { //A relationship.  Including the type of relationship and who it is with.
-            public relationshipTypes type;
-            public Object myRelationship;
-        }
 
-        public string Name { //Gets and Sets the name of the object
-            get {
-                return name;
-            }
-            set {
-                name = value;
-            }
-        }
-        public string DescFile { //Gets and Sets the file name that contains the description of the object
-            get
-            {
-                return description;
-            }
-            set {
-                description = value;
-            }
-        }
-        public string Hist { //Gets and sets the file name that contains the history of the object
-            get
-            {
-                return history;
-            }
-            set {
-                history = value;
-            }
-        }
-        public string ImageFile{ //Gets and sets the file name that contains the image of the object
-            get
-            {
-                return imageFile;
-            }
-            set {
-                imageFile = value;
-            }
-        }
-        public string[] DescText { //Gets and Sets the description text
-            get
-            {
-                string[] myDesc = File.ReadAllLines(description);
-                return myDesc;
-            }
-            set {
-                File.WriteAllLines(description, value);
-            }
-        }
-        public string[] HistText { //Gets and Sets the history text
-            get
-            {
-                string[] myHist = File.ReadAllLines(history);
-                return myHist;
-            }
-            set {
-                File.WriteAllLines(history, value); //Writes the history to the file
-            }
-        }
-        public void establishRelationship(Object relateMe, relationshipTypes myType) { //Create a new relationship
-            relationship newRelationship = new relationship();
-            newRelationship.myRelationship = relateMe;
-            newRelationship.type = myType;
-            myRelationships.Add(newRelationship);
-        }
-        public void removeRelationship(Object removeMe) { //Removes a relationship
-            foreach (relationship theRelationships in myRelationships) {
-                if (theRelationships.myRelationship == removeMe) myRelationships.Remove(theRelationships);
-            }
-        }
-        public relationshipTypes whatsTheRelationshipTo(Object myName) { //Return relationship type to given object
-            foreach (relationship theRelationship in myRelationships) {
-                if (theRelationship.myRelationship.getName() == myName.getName()) {
-                    return theRelationship.type;
-                }
-            }
-            return relationshipTypes.Null; //If the given object does not have a relationship to this object, return null type
-        }
-    }
+		protected struct relationship { //A relationship.  Including the type of relationship and who it is with.
+		    public relationshipTypes type;
+		    public Object myRelationship;
+		}
+		
+		private List<Attribute> attributes  = new List<Attribute>
+		{	new Attribute("Name", ""),
+			new Attribute("Description", ""),
+			new Attribute("Histroy", "")
+		};
+		
+		//private List<string> Gallery = new List<string> {};
+		
+		public class Attribute
+		{	public class Attribute_ID
+			{	public   Attribute_ID(string name)
+					{ called = name;	ind = (uint)AIDs.Count() + 1; }
+				public uint	  ind	{ get; set; }
+				public string called{ get; set; }
+				/*public static bool operator ==(Attribute_ID Left, Attribute_ID Right) { return (Left.ind == Right.ind); }
+				public static bool operator !=(Attribute_ID Left, Attribute_ID Right) { return (Left.ind != Right.ind); }
+				public static bool operator <=(Attribute_ID Left, Attribute_ID Right) { return (Left.ind <= Right.ind); }
+				public static bool operator >=(Attribute_ID Left, Attribute_ID Right) { return (Left.ind >= Right.ind); } //It's best to minimize these*/
+			}
+			public Attribute(string name, string txt)
+			{	foreach (Attribute_ID AID in AIDs) if (name == AID.called) Field = AID;
+				if (null == Field)
+				{	AIDs.Add(new Attribute_ID(name));/*
+				*/	Field = AIDs.Last();				}
+				text = txt;
+			}
+			public static List< Attribute_ID > AIDs;
+			public				Attribute_ID  Field = null;
+			public string						text;
+			public string[] DescText {
+				get {string[] fullDesc;
+					if (text /*is directory*/) fullDesc = File.ReadAllLines(text);
+					else fullDesc = text;
+					return fullDesc;
+				}
+				set { if(text /*is directory*/)  File.WriteAllLines(text, value);
+					else text = value;
+				}
+			} //Gets and Sets the description text
+		}
+
+		public string Name		{ get; set; }
+		public string DescFile	{ get; set; }
+		public string Hist		{ get; set; }
+		public string ImageFile	{ get; set; }
+
+		
+		public void establishRelationship(Object relateMe, relationshipTypes myType) { //Create a new relationship
+		    relationship newRelationship = new relationship();
+		    newRelationship.myRelationship = relateMe;
+		    newRelationship.type = myType;
+		    myRelationships.Add(newRelationship);
+		}
+		public void removeRelationship(Object removeMe) { //Removes a relationship
+		    foreach (relationship theRelationships in myRelationships) {
+		        if (theRelationships.myRelationship == removeMe) myRelationships.Remove(theRelationships);
+		    }
+		}
+		public relationshipTypes whatsTheRelationshipTo(Object myName) { //Return relationship type to given object
+		    foreach (relationship theRelationship in myRelationships) {
+		        if (theRelationship.myRelationship.getName() == myName.getName()) {
+		            return theRelationship.type;
+		        }
+		    }
+		    return relationshipTypes.Null; //If the given object does not have a relationship to this object, return null type
+		}
+	}
 }
