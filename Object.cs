4c8438c4a3ec5f27ea419909ee03176688f929c0
CastonYoung <caston.young@oit.edu>
2016-12-22 11:35:41 -0800

    Add files via upload
    Finished the modifiable attributes demo.

--- Object.cs
+++ Object.cs
@@ -7,67 +7,101 @@ using System.IO;
 
 namespace StoryPlanner
 {
-	public abstract class Object  //This will be a parent class to all objects
+	public class Object //: System.ComponentModel.IEditableObject  //This will be a parent class to all objects
 	{
 		public enum relationshipTypes {Null, Father, Mother, Sibling, Friend, Enemy}; //Types of relationships... let me know if we need more
 		protected string name, description, history, imageFile;
-		protected List<relationship> myRelationships; //List of this objects relationships
+		protected List<relationship> MyRelationships; //List of this objects relationships
 		
-		public Object(string name, string desc, string hist, string image_file)
+		public Object(string name, string desc, string hist, string image_file="")
 			{ InitializerA(	 name,		  desc,		   hist,		image_file); }
 
-		private InitializerA(string name, string desc, string hist, string image_file)
-		{	attribute.name = name;
-            attribute.description = desc; //Desc, hist, and imageFile hold filenames with the extension
-            attribute.history = hist;
-            this.imageFile = image_file;
-            myRelationships = new List<relationship>();
+		private void InitializerA(string name, string desc, string hist, string image_file = "") {
+			attributes.Find(x => x.Label.called == "Name"		).text = name;
+			attributes.Find(x => x.Label.called == "Description").text = desc;
+			attributes.Find(x => x.Label.called == "History"	).text = hist;
+		//	this.imageFile = image_file;
+			MyRelationships = new List<relationship>();
         }
 
 		protected struct relationship { //A relationship.  Including the type of relationship and who it is with.
 		    public relationshipTypes type;
-		    public Object myRelationship;
+		    public Object MyRelationship;
 		}
 		
-		private List<Attribute> attributes  = new List<Attribute>
-		{	new Attribute("Name", ""),
+		public List<Attribute> attributes = new List<Attribute> {
+			new Attribute("Name", ""),
 			new Attribute("Description", ""),
-			new Attribute("Histroy", "")
+			new Attribute("History", "")
 		};
 		
+		
+
 		//private List<string> Gallery = new List<string> {};
 		
-		public class Attribute
+		public class Attribute //: System.ComponentModel.IEditableObject
 		{	public class Attribute_ID
 			{	public   Attribute_ID(string name)
 					{ called = name;	ind = (uint)AIDs.Count() + 1; }
 				public uint	  ind	{ get; set; }
 				public string called{ get; set; }
-				/*public static bool operator ==(Attribute_ID Left, Attribute_ID Right) { return (Left.ind == Right.ind); }
-				public static bool operator !=(Attribute_ID Left, Attribute_ID Right) { return (Left.ind != Right.ind); }
-				public static bool operator <=(Attribute_ID Left, Attribute_ID Right) { return (Left.ind <= Right.ind); }
-				public static bool operator >=(Attribute_ID Left, Attribute_ID Right) { return (Left.ind >= Right.ind); } //It's best to minimize these*/
+				/*public static bool operator ==(Attribute_ID Left, Attribute_ID Right) { return (Left.ind	== Right.ind); }
+				public static bool operator !=(Attribute_ID Left, Attribute_ID Right) { return (Left.ind	!= Right.ind); }
+				public static bool operator <=(Attribute_ID Left, Attribute_ID Right) { return (Left.ind	<= Right.ind); }
+				public static bool operator >=(Attribute_ID Left, Attribute_ID Right) { return (Left.ind	>= Right.ind); }
+				public static bool operator ==(Attribute_ID Left, 		   int Right) { return (Left.ind	== Right	); }
+				public static bool operator !=(Attribute_ID Left,		   int Right) { return (Left.ind	!= Right	); }
+				public static bool operator <=(Attribute_ID Left,		   int Right) { return (Left.ind	<= Right	); }
+				public static bool operator >=(Attribute_ID Left,		   int Right) { return (Left.ind	>= Right	); }
+				public static bool operator ==(Attribute_ID Left,		string Right) { return (Left.called == Right	); }
+				public static bool operator !=(Attribute_ID Left,		string Right) { return (Left.called != Right	); } //It's best to minimize these*/
 			}
 			public Attribute(string name, string txt)
-			{	foreach (Attribute_ID AID in AIDs) if (name == AID.called) Field = AID;
-				if (null == Field)
-				{	AIDs.Add(new Attribute_ID(name));/*
-				*/	Field = AIDs.Last();				}
-				text = txt;
+			{	if (AIDs == null) AIDs = new List<Attribute_ID>();//Initialization
+				else foreach (Attribute_ID AID in AIDs) if (name == AID.called) Label = AID;
+				if (Label == (Attribute_ID)null)
+				{	AIDs.Add(new Attribute_ID(name));
+					Label = AIDs.Last();
+				}
+				path = txt;
 			}
-			public static List< Attribute_ID > AIDs;
-			public				Attribute_ID  Field = null;
-			public string						text;
-			public string[] DescText {
-				get {string[] fullDesc;
-					if (text /*is directory*/) fullDesc = File.ReadAllLines(text);
-					else fullDesc = text;
-					return fullDesc;
+
+			public  static List< Attribute_ID > AIDs;
+			public  			 Attribute_ID  Label = null;
+
+			private string path;
+			public  string text	//Gets and Sets the text in "path"
+			{	
+				get{string[] fullDesc;		// To be used to convert string[] to string
+					if (System.IO.Directory.Exists(path)) fullDesc = File.ReadAllLines(path);
+					else return path;
+					return string.Join(".", fullDesc);
 				}
-				set { if(text /*is directory*/)  File.WriteAllLines(text, value);
-					else text = value;
+				set{if (System.IO.Directory.Exists(path))
+					{	string[] fullDesc = { value };
+						File.WriteAllLines(path, fullDesc);
+					}else path = value;
 				}
-			} //Gets and Sets the description text
+				
+				/*public  void BeginEdit(){
+					Console.WriteLine("Start BeginEdit");
+					if (!inTxn) 
+					{
+						backupData = ;
+						inTxn = true;
+						Console.WriteLine("BeginEdit - " + this.backupData.lastName);
+					}
+					Console.WriteLine("End BeginEdit");
+				}
+				public  void EndEdit()	{}
+				public  void CancelEdit(){}*/
+			}					//Gets and Sets the text in "path"
+			public static bool operator ==(Attribute Left, string Right) { return (Left.Label.called == Right); }
+			public static bool operator !=(Attribute Left, string Right) { return (Left.Label.called != Right); }
+			public static bool operator ==(string Left, Attribute Right) { return (Left == Right.Label.called); }
+			public static bool operator !=(string Left, Attribute Right) { return (Left != Right.Label.called); }
+			//public static bool operator <=(Attribute Left,    int Right) { return (Left.Label <= Right/*.Label*/); }
+			//public static bool operator >=(Attribute Left,    int Right) { return (Left.Label >= Right/*.Label*/); } //It's best to minimize these
 		}
 
 		public string Name		{ get; set; }
@@ -75,24 +109,33 @@ namespace StoryPlanner
 		public string Hist		{ get; set; }
 		public string ImageFile	{ get; set; }
 
-		
-		public void establishRelationship(Object relateMe, relationshipTypes myType) { //Create a new relationship
+		/*public  void BeginEdit(){
+			Console.WriteLine("Start BeginEdit");
+			if (!inTxn) 
+			{
+				backupData = attributes;
+				inTxn = true;
+				Console.WriteLine("BeginEdit - " + this.backupData.lastName);
+			}
+			Console.WriteLine("End BeginEdit");
+		}
+		public  void EndEdit()	{}
+		public  void CancelEdit(){}
+		*/
+		public void EstablishRelationship(Object relateMe, relationshipTypes myType) { //Create a new relationship
 		    relationship newRelationship = new relationship();
-		    newRelationship.myRelationship = relateMe;
+		    newRelationship.MyRelationship = relateMe;
 		    newRelationship.type = myType;
-		    myRelationships.Add(newRelationship);
+		    MyRelationships.Add(newRelationship);
 		}
-		public void removeRelationship(Object removeMe) { //Removes a relationship
-		    foreach (relationship theRelationships in myRelationships) {
-		        if (theRelationships.myRelationship == removeMe) myRelationships.Remove(theRelationships);
+		public void RemoveRelationship(Object removeMe) { //Removes a relationship
+		    foreach (relationship theRelationships in MyRelationships) {
+		        if (theRelationships.MyRelationship == removeMe) MyRelationships.Remove(theRelationships);
 		    }
 		}
-		public relationshipTypes whatsTheRelationshipTo(Object myName) { //Return relationship type to given object
-		    foreach (relationship theRelationship in myRelationships) {
-		        if (theRelationship.myRelationship.getName() == myName.getName()) {
-		            return theRelationship.type;
-		        }
-		    }
+		public relationshipTypes WhatsTheRelationshipTo(Object myName) { //Return relationship type to given object
+		    foreach(relationship theRelationship in MyRelationships)
+		        { if (theRelationship.MyRelationship.Name == myName.Name) return theRelationship.type; }
 		    return relationshipTypes.Null; //If the given object does not have a relationship to this object, return null type
 		}
 	}
