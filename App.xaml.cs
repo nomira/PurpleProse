c3f2c4db6902181cb9e06744660822da14768eb5
CastonYoung <caston.young@oit.edu>
2017-01-22 18:57:05 -0800

    Add files via upload
    Edits to the text editor (to take an argument). ~Caston

---
+++ App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+
+
+namespace HelloWPF
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public static class Argument { public static string s; }
+
+	public partial class App :Application
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			Trace.WriteLine("Starting WPF app");
+			if (e.Args.Length > 0) Argument.s = e.Args[0];
+			base.OnStartup(e);
+		}
+	}
+}
